546209cc7e7f1a2db61a4116694b299d8c35b021
hgsk <hgsk85@gmail.com>
2013-09-06 17:33:06 +0900

    delete old files. fix sample controller.

--- punie/app/controllers/GreetingController.php
+++ punie/app/controllers/GreetingController.php
@@ -3,6 +3,15 @@
 class GreetingController{
 
     public function hello(){
+        /*
+         * ideal caller
+         *
+         * $user = User.find(1);
+         * $users = User.findByAge('>25');
+         * $users = User.findByNameIncludes('Nick');
+         * $courses = CourseAndSubject.findByOwnerId('1');
+         *
+         */
         $user = User.find(1);
 
         include(ROOT_PATH.'/app/views/greeting.php');
